7510091ef75441766b1652524d0c11c340e96043
supriadiroadman <supriadiroadman@gmail.com>
2019-07-20 05:28:15 +0700

    Finish

--- ci_login.sql
+++ ci_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2019 at 08:56 AM
+-- Generation Time: Jul 20, 2019 at 12:25 AM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.3.6
 
@@ -44,7 +44,52 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(6, 'Supriadi Roadman', 'supriadiroadman@gmail.com', 'default.jpg', '$2y$10$A63APU3GslX6RLpa0wgtROtnFVfJiFHzwOos3sCdtGzr5vlWd0Rd.', 2, 1, 1562999739);
+(1, 'Supriadi Roadman', 'supriadiroadman@gmail.com', 'SUPRIADI_ROADMAN_S.jpg', '$2y$10$J2JF5G43Cd/mBVxCG6b6hOBiix1eqd05y4XYUW7LnuMQqWp2b9VCu', 1, 1, 1563552505),
+(2, 'Admin', 'admin@admin.com', 'SUPRIADI_ROADMAN_S1.jpg', '$2y$10$fQXptOl9RMoq5anPqUt8OOS/M2IWaihNvvQmC70h2bI2BB09AaUBa', 1, 1, 1563001906),
+(8, 'Budi', 'budi@gmail.com', 'default.jpg', '$2y$10$8XKFu9f9q6HEz7A1kh0LJuAXtxkYN4Px1H11G62QBsXafXSRuktAO', 2, 1, 1563537292),
+(10, 'adi', 'supriadi_rey@yahoo.com', 'default.jpg', '$2y$10$XRwrjQxDeAfyGKHc.QbavOrOvJNlmpY8g0R0/KkJ0W53608nAQKzu', 2, 0, 1563549085);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(8, 2, 2),
+(10, 1, 2),
+(11, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Menu');
 
 -- --------------------------------------------------------
 
@@ -65,6 +110,58 @@ INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Administrator'),
 (2, 'Member');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1),
+(2, 2, 'My Profile', 'user', 'fas fa-fw fa-user', 1),
+(3, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 1),
+(4, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 1),
+(5, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
+(7, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1),
+(8, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_token`
+--
+
+CREATE TABLE `user_token` (
+  `id` int(11) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_token`
+--
+
+INSERT INTO `user_token` (`id`, `email`, `token`, `date_created`) VALUES
+(2, 'supriadi_rey@yahoo.com', 'SlxM60S10mM1dR6NC78a6OrLgh7YiU1W7cHXRCkFzpU=', 1563549085),
+(6, 'supriadiroadman@gmail.com', 'qEkZ2iPBGnGnve/TvTQpGE2u4iPiDRQutPU1kyNr6hU=', 1563562227),
+(7, 'supriadiroadman@gmail.com', 'h0+0FexMygw3YHIcaeSDqMzUd0WC2Vks/y0TBspI+Zg=', 1563562490),
+(8, 'supriadiroadman@gmail.com', 'T2nR14VpGOhUqgYDeF8QQuepFoGwAdL9do46CWepjxo=', 1563562881),
+(9, 'supriadiroadman@gmail.com', 'IQxuXfJcPrntMoM7oRjZWHnWN8dmeR0knGfTM1K9aIM=', 1563566486);
+
 --
 -- Indexes for dumped tables
 --
@@ -76,9 +173,33 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user_role`
 --
 ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_token`
+--
+ALTER TABLE `user_token`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -89,13 +210,37 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
